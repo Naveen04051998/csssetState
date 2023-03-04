eb0d19fdb2d0775e594a142b44d174109a0e9075
Naveen04051998 <119278490+Naveen04051998@users.noreply.github.com>
2023-03-04 12:16:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default class App extends Component {
   state={name:"naveen",
   msg:"please subscribe",
 value:"subcribe",
-backgroundColor:"red"
+backgroundColor:"red",
+num:0
 }
 
 
@@ -13,9 +14,12 @@ backgroundColor:"red"
     this.setState({value:"subscribed",msg:"thanks for subscribing",backgroundColor:"blue"})
     
     }
+   onIncrement=()=>{
+      this.setState((prevState) => ({ num: prevState.num + 1 }))
+    }
  
   render() { 
-    const {name,value,msg,backgroundColor}=this.state
+    const {name,value,msg,backgroundColor,num}=this.state
 
    
 
@@ -24,6 +28,7 @@ backgroundColor:"red"
         <h1>welcome to the screen {name}</h1>
         <p>{msg}</p>
      <button onClick={this.clicked}>{value}</button>
+<button onClick={this.onIncrement}>click</button>
       </div>
     );
   }
